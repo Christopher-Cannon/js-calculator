5ae1e8281ab5c43b28515a3afa6afc528eb8d4a7
Christopher-Cannon <cscannon92@yahoo.com>
2021-11-28 06:49:25 +0000

    Overall functionality complete

--- script.js
+++ script.js
@@ -5,17 +5,48 @@ const str = x => String(x);
 function Calculator() {
   this.ZERO = "0";
   this.expression = [];
+  this.result = "";
   this.curNumber = this.ZERO;
   this.curOperator = "";
+  this.numbers = {
+    "zero": "0",
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+  };
+  this.operators = {
+    "divide": "/",
+    "multiply": "*",
+    "subtract": "-",
+    "add": "+",
+    "pow": "^",
+  };
+  this.functions = ["log", "sqrt"];
+  this.debug = function(button) {
+    try {
+      console.log("button:", button);
+      console.log("curNumber:", this.getCurrentNumber());
+      console.log("curOperator:", this.getCurrentOperator());
+      console.log("result:", this.getResult());
+      console.log("getExpression():", this.getExpression());
+    } catch (error) {
+      console.log(error);
+    }
+  };
   this.evaluate = function() {
-    console.log(this.expression);
-    console.log(this.getExpression());
-  }
+    this.result = math.evaluate(this.getExpression());
+  };
   this.push = function(x) {
     this.expression.push(x);
   };
   this.pop = function() {
-    this.expression.pop();
+    return this.expression.pop();
   };
   this.expressionIsEmpty = function() {
     if (!this.expression.length) return true;
@@ -27,25 +58,36 @@ function Calculator() {
   this.getExpression = function() {
     return this.expression.join(" ");
   };
+  this.getResult = function() {
+    return this.result;
+  };
   this.getCurrentNumber = function() {
     return this.curNumber;
   };
-  this.appendToCurrentNumber = function(x) {
+  this.appendToCurrentNumber = function(num) {
     if (this.curNumber === this.ZERO) {
-      this.curNumber = x;
+      this.curNumber = this.numbers[num];
     } else {
-      this.curNumber += x;
+      this.curNumber += this.numbers[num];
     }
   };
-  this.storeCurrentNumber = function() {
-    this.expression.push(this.curNumber);
+  this.storeCurrentNumber = function(fn="") {
+    if (fn !== "") {
+      if (this.lastStoredWasFunction()) {
+        this.expression.push(`${fn}(${this.pop()})`);
+      } else {
+        this.expression.push(`${fn}(${this.curNumber})`);
+      }
+    } else {
+      this.expression.push(this.curNumber);
+    }
     this.curNumber = this.ZERO;
   };
   this.getCurrentOperator = function() {
     return this.curOperator;
   };
-  this.updateCurrentOperator = function(x) {
-    this.curOperator = x;
+  this.updateCurrentOperator = function(op) {
+    this.curOperator = this.operators[op];
   };
   this.storeCurrentOperator = function() {
     if (this.curOperator !== "") {
@@ -63,6 +105,15 @@ function Calculator() {
     if (!Number.isNaN(num(this.lastStored()))) return true;
     return false;
   };
+  this.lastStoredWasFunction = function() {
+    if (!this.lastStored()) return false;
+
+    for (const fn of this.functions) {
+      if (this.lastStored().startsWith(fn)) return true;
+    }
+
+    return false;
+  };
   this.back = function() {
     if (this.curNumber !== this.ZERO) {
       this.curNumber = this.curNumber.slice(0, this.curNumber.length - 1);
@@ -74,6 +125,7 @@ function Calculator() {
   this.clear = function() {
     this.expression = [];
     this.curNumber = this.ZERO;
+    this.curOperator = "";
   };
   this.clearE = function() {
     this.curNumber = this.ZERO;
@@ -83,6 +135,35 @@ function Calculator() {
   };
   this.point = function() {
     if (!this.curNumber.includes(".")) this.curNumber += ".";
+  };
+  this.number = function(num) {
+    this.storeCurrentOperator();
+    this.appendToCurrentNumber(num);
+  };
+  this.operator = function(op) {
+    if (!this.lastStoredWasFunction()) {
+      if (!this.lastStoredWasNumber() || this.expressionIsEmpty()) this.storeCurrentNumber();
+    }
+    this.updateCurrentOperator(op);
+  };
+  this.square = function() {
+    if (!this.lastStoredWasFunction()) {
+      if (!this.lastStoredWasNumber() || this.expressionIsEmpty()) this.storeCurrentNumber();
+    }
+    this.updateCurrentOperator("pow");
+    this.storeCurrentOperator();
+    this.push("2");
+  };
+  this.function = function(button) {
+    this.storeCurrentOperator();
+    this.storeCurrentNumber(button);
+  };
+  this.equals = function() {
+    // If equals is clicked after clicking an operator
+    if (this.getCurrentNumber() === "0") this.storeCurrentOperator();
+    if (!this.lastStoredWasFunction()) this.storeCurrentNumber();
+    this.evaluate();
+    this.clearExpression();
   };
 }
 
@@ -95,108 +176,81 @@ const buttons = document.querySelectorAll("button");
 output.value = c.getCurrentNumber();
 expression.textContent = c.getExpression();
 
+const displayResult = function() {
+  try {
+    c.evaluate();
+  } catch (error) {
+    console.log(`Error: couldn't evaluate (${error})`);
+  }
+  output.value = c.getResult();
+};
+
+const displayExpression = function() {
+  if (c.getCurrentOperator() !== "") {
+    expression.textContent = c.getExpression() + ` ${c.getCurrentOperator()}`;
+  } else {
+    expression.textContent = c.getExpression();
+  }
+};
+
 for (const button of buttons) {
   button.addEventListener("click", (e) => {
-    console.log(e.target.id);
-
     switch (e.target.id) {
       case "back":
         c.back();
+        output.value = c.getCurrentNumber();
         break;
       case "clear":
         c.clear();
+        output.value = c.getCurrentNumber();
         break;
       case "clearE":
         c.clearE();
+        output.value = c.getCurrentNumber();
         break;
       case "plusMinus":
         c.plusMinus();
         break;
-      case "exponent":
-        
-        break;
-      case "exponentY":
-        //if (number !== "" && number !== ZERO) operationList.push(number, "**");
+      case "sqrt":
+      case "log":
+        c.function(e.target.id);
+        displayResult();
         break;
       case "point":
         c.point();
         break;
       case "divide":
-        if (!c.lastStoredWasNumber() || c.expressionIsEmpty()) c.storeCurrentNumber();
-        c.updateCurrentOperator("/");
-        break;
       case "multiply":
-        if (!c.lastStoredWasNumber() || c.expressionIsEmpty()) c.storeCurrentNumber();
-        c.updateCurrentOperator("*");
-        break;
       case "subtract":
-        if (!c.lastStoredWasNumber() || c.expressionIsEmpty()) c.storeCurrentNumber();
-        c.updateCurrentOperator("-");
-        break;
       case "add":
-        if (!c.lastStoredWasNumber() || c.expressionIsEmpty()) c.storeCurrentNumber();
-        c.updateCurrentOperator("+");
+      case "pow":
+        c.operator(e.target.id);
+        displayResult();
+        break;
+      case "sqr":
+        c.square();
+        displayResult();
         break;
       case "equals":
-        // If equals is clicked after clicking an operator
-        if (c.getCurrentNumber() === "0") c.storeCurrentOperator();
-        c.storeCurrentNumber();
-        console.log(c.getExpression());
-        c.clearExpression();
-
-        console.log("EVALUATE EXPRESSION, CLEAR EXPRESSION, OUTPUT TOTAL");
+        c.equals();
+        output.value = c.getResult();
         break;
       case "zero":
-        c.storeCurrentOperator();
-        c.appendToCurrentNumber("0");
-        break;
       case "one":
-        c.storeCurrentOperator();
-        c.appendToCurrentNumber("1");
-        break;
       case "two":
-        c.storeCurrentOperator();
-        c.appendToCurrentNumber("2");
-        break;
       case "three":
-        c.storeCurrentOperator();
-        c.appendToCurrentNumber("3");
-        break;
       case "four":
-        c.storeCurrentOperator();
-        c.appendToCurrentNumber("4");
-        break;
       case "five":
-        c.storeCurrentOperator();
-        c.appendToCurrentNumber("5");
-        break;
       case "six":
-        c.storeCurrentOperator();
-        c.appendToCurrentNumber("6");
-        break;
       case "seven":
-        c.storeCurrentOperator();
-        c.appendToCurrentNumber("7");
-        break;
       case "eight":
-        c.storeCurrentOperator();
-        c.appendToCurrentNumber("8");
-        break;
       case "nine":
-        c.storeCurrentOperator();
-        c.appendToCurrentNumber("9");
+        c.number(e.target.id);
+        output.value = c.getCurrentNumber();
         break;
       }
-      if (c.expressionIsEmpty()) {
-        output.value = c.getCurrentNumber();
-      } else {
-        output.value = c.getCurrentNumber();
-        // output.value = c.evaluate();
-      }
-    if (c.getCurrentOperator() !== "") {
-      expression.textContent = c.getExpression() + ` ${c.getCurrentOperator()}`;
-    } else {
-      expression.textContent = c.getExpression();
-    }
+
+    // Display expression above output box
+    displayExpression();
   });
 }
